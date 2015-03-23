9c8e0a4e5a2a34d20b9c51d157f385c25724d661
Etienne DELAY <etienne.delay@unilim.fr>
2015-03-23 20:06:14 +0100

    debut de script

---
+++ script/ift_mapper.R
@@ -0,0 +1,8 @@
+## Auteur : Etienne Delay, Laboratoire Geolab, université de Limoges.
+### un scripte pour visualiser les indices de fréquence de traitement par bassin viticole d'après les données de l'agreste
+### source des données agreste : http://agreste.agriculture.gouv.fr/enquetes/pratiques-culturales/viticulture-565/
+### sources des données SIG : http://professionnels.ign.fr/geofla
+
+library(rgdal)
+library(ggplot2)
+library(plyr)
